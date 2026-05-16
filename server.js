861ea8d3c4838e533773a087eaaae5754da73a86
frank steenhoudt <frank.steenhoudt@gmail.com>
2026-05-16 16:04:40 +0200

    Fix tracker server

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-let lastLocation = null;
+let locations = {};
 
 app.use(express.json());
 app.use(express.static("public"));
 
 app.post("/api/location", (req, res) => {
-  lastLocation = {
+  const device = req.body.device || "Onbekend";
+
+  locations[device] = {
+    device,
     lat: req.body.lat,
     lng: req.body.lng,
     accuracy: req.body.accuracy,
@@ -17,13 +20,13 @@ app.post("/api/location", (req, res) => {
     sos: req.body.sos === true
   };
 
-  console.log("Nieuwe locatie:", lastLocation);
+  console.log("Nieuwe locatie:", locations[device]);
 
   res.json({ ok: true });
 });
 
 app.get("/api/location", (req, res) => {
-  res.json(lastLocation || {});
+  res.json(Object.values(locations));
 });
 
 app.listen(PORT, () => {
